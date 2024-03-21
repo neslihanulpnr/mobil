cb90116fdf0c3e966361c43eb46773cf018f5692
neslihan ulupınar <neslihanulpnr0@hotmail.com>
2024-03-21 15:29:41 +0300

    menu

--- Homepage/Menu.js
+++ Homepage/Menu.js
@@ -1,13 +1,38 @@
-import { Text, View } from "react-native"
-import { Navbar } from "./Navbar"
-import { Footer } from "./Footer"
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import { Navbar } from './Navbar';
+import { Footer } from './Footer';
+import { Home } from '../Screens/Home';
+import { Favorite } from '../Screens/Favorite';
+import { Hamper } from '../Screens/Hamper';
+import { Account } from '../Screens/Account';
 
 export const Menu = () => {
+    // İçerik state'i ve başlangıç değeri olarak Home bileşeni
+    const [content, setContent] = useState(<Home />);
+
+    // Navbar butonlarına tıklandığında içeriği değiştiren fonksiyonlar
+    const handleHomeClick = () => setContent(<Home />);
+    const handleFavoriteClick = () => setContent(<Favorite />);
+    const handleHamperClick = () => setContent(<Hamper />);
+    const handleAccountClick = () => setContent(<Account />);
+
     return (
-        <View>
-            <Navbar />
-<Text>ggsg</Text>
-            <Footer/>
+        <View style={{ flex: 1 }}>
+            <Navbar 
+                onHomeClick={handleHomeClick}
+                onFavoriteClick={handleFavoriteClick}
+                onHamperClick={handleHamperClick}
+                onAccountClick={handleAccountClick}
+            />
+            {/* İçeriği dinamik olarak değiştiren alan */}
+            {content}
+            <Footer 
+                onHomeClick={handleHomeClick}
+                onFavoriteClick={handleFavoriteClick}
+                onHamperClick={handleHamperClick}
+                onAccountClick={handleAccountClick}
+            />
         </View>
-    )
-}
+    );
+};

469be45b22d21c5d6c409c2b8883fc495cd3b0a6
neslihan ulupınar <neslihanulpnr0@hotmail.com>
2024-04-15 15:22:16 +0300

    new

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,15 +1,14 @@
-import { Text, View, StyleSheet, ScrollView, } from "react-native"
+import { Text, View, StyleSheet, ScrollView, Image } from "react-native"
 
 export const Home = () => {
     return (
         <ScrollView>
             <View>
                 <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-
                     <View style={styles.categoryList}>
                         <View style={{
-                            borderWidth: 0.7,
-                            borderColor: "grey",
+                            borderWidth: 1,
+                            borderColor: "lightgrey",
                             borderRadius: 10,
                             width: 80,
                             height: 30,
@@ -18,12 +17,12 @@ export const Home = () => {
                         }}>
                             <Text style={styles.text}>Kategoriler</Text>
                         </View>
- 
-                    <View style={{ left: 2, width: 1, height: 30, backgroundColor: "lightgrey"}}></View>
+
+                        <View style={{ left: 2, width: 1, height: 30, backgroundColor: "lightgrey" }}></View>
 
                         <View style={{
-                            borderWidth: 0.7,
-                            borderColor: "grey",
+                            borderWidth: 1,
+                            borderColor: "lightgrey",
                             borderRadius: 10,
                             width: 80,
                             alignItems: "center",
@@ -31,12 +30,12 @@ export const Home = () => {
                             backgroundColor: "#b32e2e",
                             justifyContent: "center"
                         }}>
-                            <Text style={{color: "white"}}>Kadın</Text>
+                            <Text style={{ color: "white" }}>Kadın</Text>
                         </View>
 
                         <View style={{
-                            borderWidth: 0.7,
-                            borderColor: "grey",
+                            borderWidth: 1,
+                            borderColor: "lightgrey",
                             borderRadius: 10,
                             width: 80,
                             alignItems: "center",
@@ -47,8 +46,8 @@ export const Home = () => {
                         </View>
 
                         <View style={{
-                            borderWidth: 0.7,
-                            borderColor: "grey",
+                            borderWidth: 1,
+                            borderColor: "lightgrey",
                             borderRadius: 10,
                             width: 80,
                             alignItems: "center",
@@ -60,7 +59,7 @@ export const Home = () => {
 
                         <View style={{
                             borderWidth: 0.7,
-                            borderColor: "grey",
+                            borderColor: "lightgrey",
                             borderRadius: 10,
                             width: 100,
                             alignItems: "center",
@@ -72,7 +71,7 @@ export const Home = () => {
 
                         <View style={{
                             borderWidth: 0.7,
-                            borderColor: "grey",
+                            borderColor: "lightgrey",
                             borderRadius: 10,
                             width: 100,
                             alignItems: "center",
@@ -84,17 +83,87 @@ export const Home = () => {
 
                         <View style={{
                             borderWidth: 0.7,
-                            borderColor: "grey",
+                            borderColor: "lightgrey",
                             borderRadius: 10,
                             width: 100,
                             alignItems: "center",
                             left: 22,
-                            justifyContent: "center"
+                            justifyContent: "center",
+                            marginRight: 40
                         }}>
                             <Text style={styles.text}>Anne & Çocuk</Text>
                         </View>
                     </View>
                 </ScrollView>
+
+                <View style={{ width: 395, height: 1, backgroundColor: "lightgrey" }}></View>
+
+                <View style={styles.sweat}>
+                    <Text style={styles.sweatText}>Sweatshirt</Text>
+                </View>
+
+                <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+                <View style={{flexDirection: 'row', justifyContent: "space-between"}}>
+                  <View style={{
+                    left: 15, 
+                    width: 170, 
+                    height: 250, 
+                    backgroundColor: "white", 
+                    alignItems: "center"}}>
+                    <Image source={require("../assets/black.jpeg")} style={{width: 150, height: 200}}/>
+                    <Text>dinazor desenli kadın</Text>
+                    <Text>sweatshirt</Text>
+                  </View>
+
+                  <View style={{
+                    left: 15, 
+                    width: 170, 
+                    height: 250, 
+                    backgroundColor: "white", 
+                    alignItems: "center", 
+                    left: 20}}>
+                    <Image source={require("../assets/cat.jpeg")} style={{width: 150, height: 200}}/>
+                    <Text>kadın sweatshirt</Text>
+                  </View>
+
+                  <View style={{
+                    left: 15, 
+                    width: 170, 
+                    height: 250, 
+                    backgroundColor: "white", 
+                    alignItems: "center", 
+                    left: 25}}>
+                    <Image source={require("../assets/butterfly.jpeg")} style={{width: 150, height: 200}}/>
+                    <Text>kelebek desenli kadın</Text>
+                    <Text>sweatshirt</Text>
+                  </View>
+
+                  <View style={{
+                    left: 15, 
+                    width: 170, 
+                    height: 250, 
+                    backgroundColor: "white", 
+                    alignItems: "center", 
+                    left: 30}}>
+                    <Image source={require("../assets/maneskin.jpeg")} style={{width: 150, height: 200}}/>
+                    <Text>maneskin yazılı kadın</Text>
+                    <Text>sweatshirt</Text>
+                  </View>
+
+                  <View style={{
+                    left: 15, 
+                    width: 170, 
+                    height: 250, 
+                    backgroundColor: "white", 
+                    alignItems: "center", 
+                    left: 40,
+                    marginRight: 50}}>
+                    <Image source={require("../assets/white.jpeg")} style={{width: 150, height: 200}}/>
+                    <Text>kadın sweatshirt</Text>
+                  </View>
+                </View>
+                </ScrollView>
+            
             </View>
         </ScrollView>
     )
@@ -106,7 +175,11 @@ const styles = StyleSheet.create({
         margin: 5,
         justifyContent: "space-between"
     },
-    text: {
-        
-    }
+    sweat: {
+        marginTop: 20,
+        margin: 10
+    },
+    sweatText: {
+       fontSize: 20  
+    }, 
 });
